c5a20e669d166600443670ef3ed633a0373ab4b1
beetleskin <stfn.ka@gmail.com>
2013-08-13 19:53:46 +0200

    jetz neu mit header-widget!!!

---
+++ wp-content/themes/apollo_peernet/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+
+add_action( 'after_setup_theme', 'attpeer_theme_setup' );
+add_action( 'widgets_init', 'attpeer_widgets_init' );
+
+function attpeer_theme_setup() {
+	// Localization support
+	$ret = load_child_theme_textdomain( 'att', get_stylesheet_directory() .'/lang' );
+	
+	//var_dump($ret);
+}
+
+
+function attpeer_widgets_init() {
+	register_sidebar(array(
+		'name' => __( 'Header','att'),
+		'id' => 'header-sidebar',
+		'description' => __( 'Widgets in this area are shown at the top of the page.','att' ),
+		'before_widget' => '<div class="header-widget %2$s clearfix">',
+		'after_widget' => '</div>',
+		'before_title' => '<h6>',
+		'after_title' => '</h6>',
+	));
+}
+
+
+?>

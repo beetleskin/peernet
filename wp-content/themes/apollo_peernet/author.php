5ba105f110e7dec3da856d16d9fe3abc914303e9
beetleskin <stfn.ka@gmail.com>
2013-08-18 20:58:14 +0200

    author page update

--- wp-content/themes/apollo_peernet/author.php
+++ wp-content/themes/apollo_peernet/author.php
@@ -14,6 +14,16 @@ if ( have_posts() ) : the_post(); ?>
     <div id="post" class="col span_9 clr">  
         <header id="page-heading">
             <h1><?php _e('Articles Written By','att'); ?>: <?php echo get_the_author() ?></h1>
+             <aside id="post-author-box" class="boxed clr row">
+			    <div id="post-author-image">
+			        <a href="<?php echo get_author_posts_url(get_the_author_meta( 'ID' )); ?>">
+			            <?php echo get_avatar( get_the_author_meta('user_email'), '80', '' ); ?>
+			        </a>
+			    </div><!-- #post-author-image --> 
+			    <div id="post-author-bio">
+			        <p><?php the_author_meta('description'); ?></p>
+			    </div><!-- #post-author-bio -->
+			</aside><!-- #post-author-box -->
         </header><!-- #page-heading -->
 		<?php
         rewind_posts();

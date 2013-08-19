9d464240d6a96445a9ebc992a50f132f06240575
beetleskin <stfn.ka@gmail.com>
2013-08-19 02:05:22 +0200

    colorblinder

--- wp-content/themes/apollo_peernet/footer.php
+++ wp-content/themes/apollo_peernet/footer.php
@@ -44,4 +44,61 @@
 
 <?php wp_footer(); ?>
 </body>
+
+<script type="text/javascript">//<![CDATA[
+jQuery(function($) {
+	
+	$(document).ready(function(){
+		
+		var bg_color_default = $('body').css("background-color");
+		var hue_min = 0;
+		var hue_max = 360;
+		var hue = hue_min;
+		var value = 100;
+		var value_target = 50;
+		var saturation = 0;
+		var saturation_target = 100;
+		var colorBlinder = null;
+		var idle = 0;
+		
+		function counter() {
+			idle++;
+			if(colorBlinder == null && idle > 10) {
+				console.log("init");
+				colorBlinder = window.setInterval(colorblind, 500);
+			}
+		}
+		
+		function resetIdle() {
+			idle = 0;
+			if(colorBlinder != null) {
+				clearInterval(colorBlinder);
+				colorBlinder = null;
+				$('body').css("background-color", bg_color_default);
+				hue = hue_min;
+				value = 91;
+				value_target = 40;
+				saturation = 0;
+				saturation_target = 60;
+				colorBlinder = null;
+			}
+		}
+		
+		function colorblind() {
+			hue = hue+1%hue_max;
+			value = (value <= value_target)? value_target : value-1;
+			saturation = (saturation >= saturation_target)? saturation_target : saturation+1;
+			$('body').css("background-color","hsl(" + hue + ", " + saturation + "%, " + value + "%)");
+		}
+		
+		
+		setInterval(counter, 1000);
+		$(this).click(resetIdle).mousemove(resetIdle).keypress(resetIdle);
+		
+	});
+});
+//]]></script>
+            
+            
+            
 </html>
